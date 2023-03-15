655b36ee960c60f92ba84207862b8df0a88ffb6e
kamalyes <501893067@qq.com>
2023-03-15 16:33:11 +0800

    :docs: feat：更新build文档

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 # 编译mac下可以执行文件
+# 下载工具resourcehacker，zip版本解压（http://www.angusj.com/resourcehacker/）
+# 加图标命令：ResourceHacker -open main.exe -save output.exe -action addskip -res main.ico -mask ICONGROUP,MAIN,
+
 go build -ldflags "-s -w" -o gst-mac main.go
 
 # 使用交叉编译 linux和windows版本可以执行的文件
